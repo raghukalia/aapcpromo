eddd4f26dae848091f778991d06e7fd620a6ba70
Raghu Kalia <raghu.kalia@aapc.com>
2023-06-27 00:04:58 +0530

    format

--- pages/promofiles/[slug].js
+++ pages/promofiles/[slug].js
@@ -5,67 +5,65 @@ import Image from "next/image"
 const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_KEY
-  })
-  export const getStaticPaths= async()=>{
-    const res=await client.getEntries({
-        content_type:'product'
+})
+export const getStaticPaths = async () => {
+    const res = await client.getEntries({
+        content_type: 'product'
     })
-    const paths= res.items.map(item => {
-return{
-    params:{slug: item.fields.slug}
-}
+    const paths = res.items.map(item => {
+        return {
+            params: { slug: item.fields.slug }
+        }
     })
-    return{
+    return {
         paths,
-        fallback:true
+        fallback: true
     }
-  }
-  export async function getStaticProps({params}){
-    const {items}=await client.getEntries({
-        content_type:'product',
-        'fields.slug':params.slug
+}
+export async function getStaticProps({ params }) {
+    const { items } = await client.getEntries({
+        content_type: 'product',
+        'fields.slug': params.slug
     })
-    if(!items.length){
-        return{
-            redirect:{
-                desination:'/',
-                permanent:false
+    if (!items.length) {
+        return {
+            redirect: {
+                desination: '/',
+                permanent: false
             }
         }
     }
-    return{
-        props:{product:items[0]},
-        revalidate: 1 
+    return {
+        props: { product: items[0] },
+        revalidate: 1
     }
-  }
-export default function ProductDetails({product}){
-    const { productTitle,productDescription,thumbnail,slug} = product.fields;
+}
+export default function ProductDetails({ product }) {
+    const { productTitle, productDescription, thumbnail, slug } = product.fields;
     console.log(product)
-    return(
+    return (
         <div>
-           
-              <div className="card mb-3">
-  <div className="row g-0">
-    <div className="col-md-4">
-    <Image 
-                src={'https:'+thumbnail.fields.file.url}
-                width={thumbnail.fields.file.details.image.width}
-                height={thumbnail.fields.file.details.image.height}
-                style={{ maxWidth: '100%' }}
-                alt={productTitle}
-              />
-    </div>
-    <div className="col-md-8">
-      <div className="card-body">
-        <h5 className="card-title">{productTitle}</h5>
-        <p className="card-text">{documentToReactComponents(productDescription)}</p>
-      <Link href="/" className="btn btn-primary">Back to Promo</Link>
-      </div>
-    </div>
 
-
-  </div>
-</div>
+            <div className="card mb-3">
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <Image
+                            src={'https:' + thumbnail.fields.file.url}
+                            width={thumbnail.fields.file.details.image.width}
+                            height={thumbnail.fields.file.details.image.height}
+                            style={{ maxWidth: '100%' }}
+                            alt={productTitle}
+                        />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{productTitle}</h5>
+                            <p className="card-text">{documentToReactComponents(productDescription)}</p>
+                            <Link href="/" className="btn btn-primary">Back to Promo</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
